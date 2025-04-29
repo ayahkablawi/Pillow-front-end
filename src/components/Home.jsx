5fb66e7919eb450abac4a29121c9032903ba583a
User Name <user@email.com>
2025-04-29 10:12:12 -0500

    Initial commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+// src/components/Home.jsx
+import React from 'react';
+
+const Home = () => {
+  return (
+    <main style={{ textAlign: 'center', padding: '2rem' }}>
+      <h1>Welcome to Pillow</h1>
+      <p>Find your dream home today.</p>
+
+    </main>
+  );
+};
+
+export default Home;
